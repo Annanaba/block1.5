4e85743e566ccd9177c8ce74801409703403b0bc
Annanaba <navahaba0@gmail.com>
2024-03-06 17:05:40 +0900

    П

--- js.js
+++ js.js
@@ -1,28 +1,4 @@
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: true,
 
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//     },
-
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-
-//     // And if we need scrollbar
-//     scrollbar: {
-//         el: '.swiper-scrollbar',
-//     },
-// });
-
-
-
-// логика кнопки скрыть
 const hideBotton = document.querySelector('.main__description_btn');
 
 const hiddenSlides = document.querySelector('.brend-list');
